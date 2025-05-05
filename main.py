3cdd063a469161211627724613e83e4a60460069
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-05-05 17:28:27 +0530

    defining colors

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from tkinter import Tk, filedialog, Label, Button, Entry, StringVar, OptionMenu
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 
+# Global to store final image
+watermarked_img = None
+
+# Define some fun colors for text
+colors = {
+    "White": (255, 255, 255, 180),
+    "Red": (255, 0, 0, 180),
+    "Blue": (0, 0, 255, 180),
+    "Green": (0, 255, 0, 180),
+    "Yellow": (255, 255, 0, 180),
+    "Pink": (255, 105, 180, 180)
+}
+
+
+
